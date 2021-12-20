e12f09ce04a6bd239aa03838293545b3276a0381
tadsmyth <tadsmith7@gmail.com>
2021-12-20 16:17:23 -0600

    basic look of sidebar MVP

--- src/components/SideNavbar.js
+++ src/components/SideNavbar.js
@@ -10,8 +10,20 @@ function Navbar(props) {
     const [submenu, setSubmenu] = useState('false');
 
     return (
-        <>
-        {/* when the screen is regular and isnt clicked the arrow will show to the left and all the nav stuff will show */}
+<>
+        <div className="d-flex flex-column vh-50 flex-shrink-0 p-3 text-white bg-dark mt-3">
+    <hr/>
+    <br />
+    <ul class="nav nav-pills flex-column mb-auto">
+        <li> <a href="#" class="nav-link text-white"> <span class="ms-2"><MdOutlineSpaceDashboard size={25}/>  Dashboard</span> </a> </li>
+        <li> <a href="#" class="nav-link text-white"> <span class="ms-2"><FaRegCalendar size={22}/>   Today</span> </a> </li>
+        <li> <a href="#" class="nav-link text-white"> <span class="ms-2"><FaRegCalendarAlt size={22}/>   Upcoming</span> </a> </li>
+    </ul>
+</div>
+
+
+
+ {/* when the screen is regular and isnt clicked the arrow will show to the left and all the nav stuff will show */}
         {/* as the screen gets smaller or the arrow is clicked the arrow will switch to the right and only the nav icons will show */}
         {/* click on the arrow works you just have to add the css */}
         <div className={`side-bar ${unclick ? 'unclick' : ''}`}>
@@ -19,44 +31,9 @@ function Navbar(props) {
                 {/* when you click this another menu btn will show */}
                 {unclick ? <BsFillArrowRightSquareFill/> : <BsFillArrowLeftSquareFill /> }
             </div>
-            {/* wasnt sure if you wanted to add this you can remove the search bar if youd like  you can keep it in the header or the Nav*/}
-            <div className="searchBar">
-                <input type="text" placeholder="search"/>
-            <button className="searchBtn">
-                <BsSearch />
-            </button>
-            </div>
-            {/* the thin line that seberates the sections */}
-            <div className="line"></div>
+           
 
-        <div>
-            <ul>
-                <li>
-                    <a className="menu">
-                        <div classNmae="menuIcons">
-                            <MdOutlineSpaceDashboard />
-                        </div>
-                            <span>Dashboard</span>
-                    </a>
-                </li>
-                <li>
-                    <a className="menu">
-                        <div classNmae="menuIcons">
-                            <FaRegCalendar />
-                        </div>
-                            <span>Today</span>
-                    </a>
-                </li>
-                <li>
-                    <a className="menu">
-                        <div classNmae="menuIcons">
-                            <FaRegCalendarAlt />
-                        </div>
-                            <span>Upcoming</span>
-                    </a>
-                </li>
-            </ul>
-        </div>
+       
             {/* im the side bar */}
             <ul ClassName="navbar">
                 {/* stretch goal */}
@@ -83,7 +60,15 @@ function Navbar(props) {
             {/* adding the project components will go here I have to add them */}
             </div>
         </div>
-        </>
+
+
+
+
+
+
+
+</>
+
     );
 }
 
