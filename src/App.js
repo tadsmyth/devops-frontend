5a99444f4e62d25b5a8c3e037c64faac932da9c5
The-RedEye <setswipe@gmail.com>
2021-12-21 22:33:14 -0500

    Implemented useContext created fetch component (optional), but trying to make it run in initial load :: Won't fetch unless code is modified and saved. need help added Context component added semi-functional fetch for user, projects and tasks

--- src/App.js
+++ src/App.js
@@ -4,15 +4,54 @@ import SideNavbar from './components/SideNavbar';
 import Header from './components/Header';
 import Contents from './components/Contents';
 import CreatingTasks from './components/bodyComponents/CreatingTasks';
-
+import dataContext from './components/Context';
+import Fetch from './components/Fetch';
+import {useState, useEffect} from 'react'
 
 function App() {
+
+const [devs, setDevs] = useState([])
+const [projects, setProjects] = useState([])
+const [tasks, setTasks] = useState([])
+
+// const url = "http://localhost:4000/"
+
+// useEffect(() => {
+//   //fetches projects
+//   fetch(`${url}devops`)
+//       .then((res) => res.json())
+//       .then((res) => {setProjects(res)
+//       console.log("Projects Data:", projects)
+//       })
+//       .catch(console.err);
+
+//   //fetches users
+//   fetch(`${url}user`)
+//       .then((res) => res.json())
+//       .then((res) => {setDevs(res)
+//       console.log("Dev Data:", devs)
+//       })
+//       .catch(console.err);
+
+//   //fetches tasks
+//   fetch(`${url}task`)
+//       .then((res) => res.json())
+//       .then((res) => {setTasks(res)
+//       console.log("Task/ToDo Data:", tasks)
+//       })
+//       .catch(console.err);
+
+//   }, [])
+
   return (
     <div className="App">
-      <h1>Todo</h1>
-      <Header />
-      <Contents />
-      <CreatingTasks />
+      <dataContext.Provider value={{devs, setDevs, projects, setProjects, tasks, setTasks}}>
+          <Fetch />
+          <h1>Todo</h1>
+          <Header />
+          <Contents />
+          <CreatingTasks />
+      </dataContext.Provider>
     </div>
   );
 }
