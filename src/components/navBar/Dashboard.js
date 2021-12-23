10a5e11e0fda36d5257714a15c371953a05e7015
tadsmyth <tadsmith7@gmail.com>
2021-12-23 14:56:21 -0600

    reduced card column to one card 1

--- src/components/navBar/Dashboard.js
+++ src/components/navBar/Dashboard.js
@@ -39,6 +39,7 @@ function Dashboard(props) {
                 <CreatingTasks />
             </div>
             
+<<<<<<< HEAD
             <div className='container'>
                 <div class="container fluid p-5 mt-5 bg-warning w-25 mw-75">
                  <div class="card-columns">
@@ -70,11 +71,26 @@ function Dashboard(props) {
                     </div>
 
 </div>
+=======
+            
+                <div class="container fluid p-5 mt-5 bg-danger w-25 mw-75">
+                <div class="card-columns">
+                                    <div class="card">
+                                        <div class="card-body">
+                                        <h5 class="card-title">Card title that wraps to a new line</h5>
+                                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        </div>
+                                    </div>
+                </div>
+
+                </div>
+>>>>>>> f87845c (reduced card column to one card 1)
 
 
 
 
 
+<<<<<<< HEAD
 <div class="container p-5 mt-5 bg-danger w-25 mw-50">
 <div class="card-columns">
                     <div class="card">
@@ -108,14 +124,8 @@ function Dashboard(props) {
                         </div>
                     </div>
                     </div>
-
-                </div>
-
-
-
-
-
-                <div class="container p-5 mt-5 bg-success w-25 mw-50">
+=======
+                <div class="container fluid p-5 mt-5 bg-warning w-25 mw-75">
                 <div class="card-columns">
                                     <div class="card">
                                         <div class="card-body">
@@ -123,31 +133,24 @@ function Dashboard(props) {
                                         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                         </div>
                                     </div>
+                </div>
+>>>>>>> f87845c (reduced card column to one card 1)
+
+                </div>
+
+
+
+
+
+                <div class="container fluid p-5 mt-5 bg-success w-25 mw-75">
+                <div class="card-columns">
                                     <div class="card">
                                         <div class="card-body">
-                                        <h5 class="card-title">Card title</h5>
-                                        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                        <h5 class="card-title">Card title that wraps to a new line</h5>
+                                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                         </div>
                                     </div>
-                                    <div class="card bg-primary text-white text-center p-3">
-                                        <blockquote class="blockquote mb-0">
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat.</p>
-                                        <footer class="blockquote-footer">
-                                            <small>
-                                            Someone famous in <cite title="Source Title">Source Title</cite>
-                                            </small>
-                                        </footer>
-                                        </blockquote>
-                                    </div>
-                                    <div class="card text-center">
-                                        <div class="card-body">
-                                        <h5 class="card-title">Card title</h5>
-                                        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                                        </div>
-                                    </div>
-                                    </div>
+                </div>
 
                 </div>
                 </div>
