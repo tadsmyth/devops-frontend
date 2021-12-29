1044e79aa3d50ee5f0368205161a19fd549cec1c
Mabe-Diaby <diabymabe@gmail.com>
2021-12-28 21:12:47 -0500

    sideNav bar

--- src/components/SideNavbar.js
+++ src/components/SideNavbar.js
@@ -1,91 +1,149 @@
-import React, {useState} from 'react';
-import {FaChevronRight, FaChevronDown, FaInbox, FaRegCalendarAlt, FaRegCalendar, FaCog, FaPlus, FaCheck} from 'react-icons/fa'
-import {MdOutlineSpaceDashboard} from 'react-icons/md'
-import {Link} from "react-router-dom";
-import {BsFillArrowLeftSquareFill, BsSearch,BsFillArrowRightSquareFill} from 'react-icons/bs'
-import '../App.css'
-import Today from '../components/navBar/Today'
-import Adding from '../components/todoModal/Adding'
-import Dashboard from '../components/navBar/Dashboard'
-
+import React, { useState } from "react";
+import {
+  FaChevronRight,
+  FaChevronDown,
+  FaInbox,
+  FaRegCalendarAlt,
+  FaRegCalendar,
+  FaCog,
+  FaPlus,
+  FaCheck,
+} from "react-icons/fa";
+import { MdOutlineSpaceDashboard } from "react-icons/md";
+import { Link } from "react-router-dom";
+import {
+  BsFillArrowLeftSquareFill,
+  BsSearch,
+  BsFillArrowRightSquareFill,
+} from "react-icons/bs";
+import { GrClose } from "react-icons/gr";
+import "../App.css";
+import Today from "../components/navBar/Today";
+import Adding from "../components/todoModal/Adding";
+import Dashboard from "../components/navBar/Dashboard";
+import "./sideNav.css";
+import TodoModal from "./todoModal/TodoModal";
+import { AiOutlinePlus, AiOutlineMenu } from "react-icons/ai";
 
 function Navbar(props) {
-    // const [showLinks, setShowLinks] = useState(false);
-    const [unclick, setUnclick] = useState('false');
-    const [submenu, setSubmenu] = useState('false');
+  // const [showLinks, setShowLinks] = useState(false);
+  const [unclick, setUnclick] = useState("false");
+  const [submenu, setSubmenu] = useState("false");
+  const [modalShow, setModalShow] = React.useState(false);
 
-
+  const [inactive, setInactive] = useState(false);
 
   // Menu hide and return button/function??? - Tad
 
-    return (
-<>
-<div className='wrapper'>
-    {/* wrapper contains sidebar */}
-    <div className='sidebar'>
-        <div className='user'>
-            <a href='users'> <img src='https://randomuser.me/api/portraits/men/11.jpg' alt='profile pic'/></a>
-            <h3>John Smith</h3>
-        </div>
+  return (
+    <>
+      <div className="navFile">
+        {/* <div className="navBody"> */}
+        <div className={`${inactive ? 'inactive' : 'wrapper'}`}>
+          <div className="contain" id="contain">
+            <aside>
+              {/* wrapper contains sidebar */}
+              <div className="sidebar">
+                <div className="user">
+                    <div onClick={() => setInactive(!inactive)} className="toggleBtn">
+                        {inactive ? <BsFillArrowRightSquareFill /> : <BsFillArrowLeftSquareFill />}
+                    </div>
+                {/* <BsFillArrowRightSquareFill /> */}
+                  {/* <a href="users">
+                    {" "} */}
+                    {inactive ? <a href="users">
+                    {" "} <img
+                      className="navImg"
+                      src="https://randomuser.me/api/portraits/men/11.jpg"
+                      alt="profile pic"
+                    />
+                  </a> : <a href="users">
+                  {" "} <img
+                    className="navImg"
+                    src="https://randomuser.me/api/portraits/men/11.jpg"
+                    alt="profile pic"
+                    /> 
+                </a>
+                }
+                {inactive ? '' : <h2 className="userName" >John Smith</h2>}
+                    {/* <img
+                      className="navImg"
+                      src="https://randomuser.me/api/portraits/men/11.jpg"
+                      alt="profile pic"
+                    />
+                  </a>
+                  <h3 className="userName">John Smith</h3> */}
+                </div>
 
-        {/* menu items in here */}
-        <ul className='list-group'>
-                
-                {/* <a href="#" class="list-group-item list-group-item-action active p-3">
+                {/* menu items in here */}
+                <ul className="list-group">
+                  {/* <a href="#" class="list-group-item list-group-item-action active p-3">
                        <MdOutlineSpaceDashboard size={25} className='m-2'/>
                        Dashboard
                     </a> */}
-                    {/* isnt working not sure why */}
-                    
-            <Link to="/" className="list-group-item list-group-item-action active p-3">
-                <MdOutlineSpaceDashboard size={25} className='m-2'/>
-                Dashboard
-            </Link>
+                  {/* isnt working not sure why */}
 
-            <Link to="/today">
-            <li class="list-group-item list-group-item-action active p-3">
-                <FaRegCalendar size={22} className='m-2'/>
-                Today
-            </li>
-            </Link> 
-            
-            <Link to="/upcoming">
-            <li class="list-group-item list-group-item-action active p-3">
-                <FaRegCalendarAlt size={22} className='m-2'/>
-                Upcoming
-            </li>
-            </Link> 
-            
-            <Link to="/settings">
-            <li class="list-group-item list-group-item-action active p-3">
-                <FaCog size={22} className='m-2' />
-                Settings
-            </li>
-            </Link> 
-            
-            <a href="#" class="list-group-item list-group-item-action active p-3">
-                <FaPlus size={22} className='m-2' />
-                New Project
-            </a>
-                    
-            <Link to="/completed">
-            <li class="list-group-item list-group-item-action active p-3">
-                <FaCheck size={22} className='m-2' />
-                Completed
-            </li>
-            </Link>
-            
-            <li class="list-group-item list-group-item-action active p-3">
-                <Adding size={22} className='m-2' />
-            </li>
-                                
-        </ul>
+                  {inactive ? <Link to="/" className="navsInactive">
+                    <MdOutlineSpaceDashboard />
+                  </Link> : <Link to="/" className="navs">
+                    <MdOutlineSpaceDashboard />
+                    <span className="taskName"> Dashboard</span>
+                  </Link>}
 
-    </div>
-</div>
+                  {inactive ? <Link to="/today" className="navsInactive">
+                  <FaRegCalendar />
+                  </Link> : <Link to="/today" className="navs">
+                    <FaRegCalendar />
+                    <span className="taskName"> Today</span>
+                  </Link>}
 
+                  {inactive ? <Link to="/upcoming" className="navsInactive">
+                    <FaRegCalendarAlt />
+                  </Link> : <Link to="/upcoming" className="navs">
+                    <FaRegCalendarAlt />
+                    <span className="taskName"> Upcoming</span>
+                  </Link>}
 
-{/* <div className="d-flex flex-column vh-50 flex-shrink-0 p-3 text-white bg-dark mt-3">
+                  {inactive ? <Link to="/settings" className="navsInactive">
+                    <FaCog />
+                  </Link> : <Link to="/settings" className="navs">
+                    <FaCog />
+                    <span className="taskName"> Settings</span>
+                  </Link>}
+
+                  {inactive ? <a href="#" className="navsInactive">
+                    <FaPlus />
+                  </a> : <a href="#" className="navs">
+                    <FaPlus />
+                    <span className="taskName"> New Project</span>
+                  </a>}
+
+                  {inactive ? <Link to="/completed" className="navsInactive">
+                    <FaCheck />
+                  </Link> : <Link to="/completed" className="navs">
+                    <FaCheck />
+                    <span className="taskName"> Completed</span>
+                  </Link>}
+
+                  {inactive ? <li className="navsInactive" onClick={() => setModalShow(true)}>
+                      <AiOutlinePlus />
+                  </li> : <li className="navs" onClick={() => setModalShow(true)}>
+                      <AiOutlinePlus />
+                      <span className="taskName"> Add Task</span>
+                  </li>}
+
+                  <TodoModal
+                    show={modalShow}
+                    onHide={() => setModalShow(false)}
+                  />
+                </ul>
+              </div>
+            </aside>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="d-flex flex-column vh-50 flex-shrink-0 p-3 text-white bg-dark mt-3">
     <hr/>
     <br />
     <ul class="nav nav-pills flex-column mb-auto">
@@ -95,30 +153,26 @@ function Navbar(props) {
     </ul>
 </div> */}
 
+      {/* when the screen is regular and isnt clicked the arrow will show to the left and all the nav stuff will show */}
+      {/* as the screen gets smaller or the arrow is clicked the arrow will switch to the right and only the nav icons will show */}
+      {/* click on the arrow works you just have to add the css */}
 
+      {/* im the side bar */}
+      <ul ClassName="navbar">
+        {/* stretch goal */}
+        {/* <li><FaInbox /> Inbox</li> */}
+        {/* brings in tasks due today */}
 
- {/* when the screen is regular and isnt clicked the arrow will show to the left and all the nav stuff will show */}
-        {/* as the screen gets smaller or the arrow is clicked the arrow will switch to the right and only the nav icons will show */}
-        {/* click on the arrow works you just have to add the css */}
-           
-
-       
-            {/* im the side bar */}
-            <ul ClassName="navbar">
-                {/* stretch goal */}
-                {/* <li><FaInbox /> Inbox</li> */}
-                {/* brings in tasks due today */}
-
-                {/* brings in upcoming tasks when clicked */}
-            {/* <li className="menu" onClick={() => setShowLinks(!showLinks)}>☰</li> */}
-            </ul>
-            {/* <div className={`projectNav ${submenu ? 'submenu' : ''}`}>
+        {/* brings in upcoming tasks when clicked */}
+        {/* <li className="menu" onClick={() => setShowLinks(!showLinks)}>☰</li> */}
+      </ul>
+      {/* <div className={`projectNav ${submenu ? 'submenu' : ''}`}>
                 <div onClick={() => {setSubmenu(!submenu)}}>
                 {submenu ? <FaChevronRight /> : <FaChevronDown />}
                 <h2>Projects</h2>
                 </div> */}
-            {/* <ul className="dummyProjects">Projects will be here</ul> */}
-            {/* <div onClick={() => {setSubmenu(!submenu)}}>
+      {/* <ul className="dummyProjects">Projects will be here</ul> */}
+      {/* <div onClick={() => {setSubmenu(!submenu)}}>
                 {submenu ? <FaChevronRight /> : <FaChevronDown />}
                 <h2>Label</h2>
                 </div>
@@ -126,12 +180,10 @@ function Navbar(props) {
                 {submenu ? <FaChevronRight /> : <FaChevronDown />}
                 <h2>Filter</h2>
                 </div> */}
-            {/* adding the project components will go here I have to add them */}
-            {/* </div> */}
-
-</>
-
-    );
+      {/* adding the project components will go here I have to add them */}
+      {/* </div> */}
+    </>
+  );
 }
 
 export default Navbar;
